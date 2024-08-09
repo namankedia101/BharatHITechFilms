5dee70c3e18265f2bb66bdc74c80f0463de8b031
Naman Kedia <kediaarts@gmail.com>
2024-08-09 17:05:04 +0530

    Add files via upload

---
+++ assets/data/productsData.js
@@ -0,0 +1,310 @@
+const productsData = [
+  {
+    id: 1,
+    productName: 'Release Liner',
+    productImg: require('../images/release-liner/1.jpg'),
+    productImages: [
+      require('../images/release-liner/1.jpg'),
+      require('../images/release-liner/2.jpg'),
+      require('../images/release-liner/3.jpg'),
+    ],
+
+    desc: [
+      'Release Liner film is coated on single side or both side with silicon to enhance the release feature. This product is suitable for many release applications.',
+      'Type of Release Liner Films we offer:',
+      '-> Transparent',
+      '-> Hazy',
+      '-> Metallized',
+    ],
+    advantages: [
+      'boosted mechanical strength',
+      'excellent chemical resistance',
+      'Good optical clarity',
+      'Recyclable',
+      'Bonds well to metal, adhesives, and variety of inks',
+      'Dimensional Stability',
+      'Tensile Properties',
+      'Heat resistance',
+    ],
+    applications: [
+      'Labels',
+      'Adhesive Tapes',
+      'Holograms',
+      'Electronic components ',
+      'Vinyls, Graphical arts',
+      'roofing sheets',
+      'Electronic die cutting',
+    ],
+  },
+
+  {
+    id: 2,
+    productName: 'Polyester Films',
+    productImg: require('../images/polyester-film/1.jpeg'),
+    productImages: [
+      require('../images/polyester-film/1.jpeg'),
+      require('../images/polyester-film/2.jpeg'),
+      require('../images/polyester-film/3.jpeg'),
+    ],
+
+    desc: [
+      'PET film is a high-performance plastic film with several unique properties that are required form various end-uses. PET film is available commercially in a range of widths, thicknesses and properties depending upon the needs of end users.',
+      'Type of PET Films we offer:',
+      '-> Transparent',
+      '-> Milky',
+      '-> Matte',
+      '-> Hazy',
+      '-> Metallized',
+      '-> Corona Treated',
+      '-> Chemical Treated',
+    ],
+    advantages: [
+      'Higher tensile strength',
+      'Inert to most chemicals',
+      'Excellent dimensional stability',
+      'Excellent electrical properties',
+      'Good thermal stability over wide temperature range',
+      'Low moisture absorption',
+      'Superior UV resistance',
+      'Good optical clarity',
+      'Recyclable',
+      'Bonds well to metal, adhesives, and variety of inks',
+    ],
+    applications: [
+      'Surface protection for windows, screens, and merchandizing displays',
+      'Pressure-sensitive labels and tags',
+      'Thermal lamination',
+      'Wire and cable insulation',
+      'Electrical components',
+      'Electrical insulation',
+      'Packaging',
+    ],
+  },
+  {
+    id: 3,
+    productName: 'DTF Films',
+    productImg: require('../images/Dtf-film/1.jpeg'),
+    productImages: [
+      require('../images/Dtf-film/1.jpeg'),
+      require('../images/Dtf-film/2.jpeg'),
+      require('../images/Dtf-film/3.jpeg'),
+      require('../images/Dtf-film/5.jpeg'),
+      require('../images/Dtf-film/6.jpeg'),
+      require('../images/Dtf-film/7.jpeg'),
+      require('../images/Dtf-film/8.jpeg'),
+      require('../images/Dtf-film/9.jpeg'),
+    ],
+
+    desc: [
+      'Direct transfers film (DTF) heat transfers are a great solution for your custom apparel full colour graphics and it is a new technology. These heat transfers are digitally printed and come ready to apply.',
+    ],
+    advantages: [
+      'Easy and faster, peel right after heat pressing',
+      'Highly cost effective',
+      'Works with almost all types of fabrics',
+      'Suitable for medium ink volume',
+      'Adapt to different humidity and temperature',
+      'Easy printing with 3 passes',
+      'Soft feel and durable',
+      'Environmental friendly and suitable for pigment ink',
+    ],
+    applications: [
+      'Washing labels for garments',
+      'Fashion clothes (most kinds of garments and no need to distinguish colours)',
+      'Personalized customization',
+      'Customized wood products',
+    ],
+  },
+  {
+    id: 4,
+    productName: 'Foilmylar',
+    productImg: require('../images/foilmylar/1.jpeg'),
+    productImages: [
+      require('../images/foilmylar/1.jpeg'),
+      require('../images/foilmylar/2.jpeg'),
+    ],
+
+    desc: [
+      'Foilmylar laminates are composites of Aluminum Foil & Polyester Film. They are glued together with superior adhesives. The Polyester Film provides the mechanical support to the more fragile Aluminum foil which is the active shielding / screening layer of the product.',
+    ],
+    advantages: [
+      'Higher Mechanical Strength as compound to Aluminum Foil',
+      'Good Shielding / Screen Properties',
+      'High electrical resistivity properties',
+    ],
+    applications: [
+      'Cable insulation',
+      'Refrigeration',
+      'Air conditioning industry ',
+      'Sealing air duct cable and air pipes',
+      'Binding duct insulation',
+      'Labels',
+    ],
+  },
+  {
+    id: 5,
+    productName: 'Metalized Films',
+    productImg: require('../images/Metalized-film/1.jpeg'),
+    productImages: [
+      require('../images/Metalized-film/1.jpeg'),
+      require('../images/Metalized-film/2.jpeg'),
+      require('../images/Metalized-film/3.jpeg'),
+    ],
+
+    desc: [
+      'One of the most important materials for the flexible packaging market. We offer all kind of metallized films for food and non-food industries. Within our wide assortment, you will find an extended range of qualities and possible applications.',
+      'Type of Metallized PET Films we offer:',
+      '-> MET-PET',
+      '-> MET-BOPP',
+    ],
+    advantages: [
+      'higher toughness',
+      'ability to be heat sealed',
+      'lower density',
+      'lower cost as aluminium foil',
+    ],
+    applications: [
+      'high barrier Flexible',
+      'Printing and Packaging ',
+      'metallic yarn',
+      'sequins for textiles',
+      'decoratives',
+      'gift wraps',
+    ],
+  },
+  {
+    id: 6,
+    productName: 'Leading Films',
+    productImg: require('../images/Leading-film/1.jpeg'),
+    productImages: [
+      require('../images/Leading-film/1.jpeg'),
+      require('../images/Leading-film/2.jpeg'),
+      require('../images/Leading-film/3.jpeg'),
+    ],
+
+    desc: [
+      'Leading films are most often used in food packaging applications as the leading companion to plastic, cardboard, or metal trays and general rigid containers. Unlike tray overwrap that wraps over the container, leading films are sealed to the accompanying container via a sealing machine and serves as a protective barrier that keeps the food separate from any potentially harmful substances.',
+    ],
+    advantages: [
+      'Clean and easy peeling',
+      'Consumer safety',
+      'Product protection and quality preservation',
+      'Brand integration',
+      'Sustainable advantages',
+    ],
+    applications: [
+      'Frozen food packaging',
+      'sealing food trays',
+      'Ready to eat, meat & dairy products',
+    ],
+  },
+  {
+    id: 7,
+    productName: 'Stretch Films',
+    productImg: require('../images/stretch-film/1.jpeg'),
+    productImages: [
+      require('../images/stretch-film/1.jpeg'),
+      require('../images/stretch-film/2.jpeg'),
+      require('../images/stretch-film/3.jpeg'),
+    ],
+
+    desc: [
+      'Our stretch films are manufactured from fine quality polyolefin and are easy to use. Owing to its effective temperature resistance and maximum durability, these films are widely preferred by various numbers of clients. Ldpe Stretch film supplied by us is of high quality with great strength and thickness.',
+    ],
+    advantages: [
+      'Packaging Waste Reduction',
+      'Durable',
+      'Humidity and heat resistance',
+      'Shipping Cost Reductions',
+      'Higher tensile strength',
+      'Low moisture absorption',
+    ],
+    applications: ['Transportation packaging', 'Storage packaging '],
+  },
+  {
+    id: 8,
+    productName: 'Paint Protection Films',
+    productImg: require('../images/paint-protection-film/1.jpeg'),
+    productImages: [
+      require('../images/paint-protection-film/1.jpeg'),
+      require('../images/paint-protection-film/2.jpeg'),
+      require('../images/paint-protection-film/3.jpeg'),
+      require('../images/paint-protection-film/4.jpeg'),
+    ],
+
+    desc: [
+      'Paint Protection Film is one of the highly sought after and an ideal protection for your Car’s Original paint. These are fine sheets which shields the car from wear and tear caused due to fine debris, swirls, splash, stains and in turn, increase the surface durability. In short PPF is nothing but a cosmetic security for your car. PPF is best to protect external painted surface and gloss that retains for long.',
+    ],
+    advantages: [
+      'High Gloss and Shine',
+      'Scratch Protection',
+      'Reduces Needs for Washing',
+    ],
+    applications: ['Automotive '],
+  },
+  {
+    id: 9,
+    productName: 'Car Wraps',
+    productImg: require('../images/car-wraps/1.jpeg'),
+    productImages: [
+      require('../images/car-wraps/1.jpeg'),
+      require('../images/car-wraps/2.jpeg'),
+      require('../images/car-wraps/3.jpeg'),
+      require('../images/car-wraps/4.jpeg'),
+      require('../images/car-wraps/5.jpeg'),
+    ],
+
+    desc: [
+      'We offer an unparalleled combination of shades and finishes to create truly unique and innovative designs. These vinyl graphics also boasts features that make vinyl graphic installation quick and efficient, resulting in vehicle wraps that will conquer the competition and wow your customers at every turn.',
+    ],
+    advantages: [
+      'Cool Options',
+      'Fast Process',
+      'Paint Protection',
+      'Easy Maintenance',
+      'You Save Money',
+      'Easy Removal',
+      'Marketing Opportunities',
+      'Easy Upgrade',
+    ],
+    applications: ['Automotive', 'Advertising'],
+  },
+  {
+    id: 10,
+    productName: 'Thermal Films',
+    productImg: require('../images/thermal-films/1.jpeg'),
+    productImages: [
+      require('../images/thermal-films/1.jpeg'),
+      require('../images/thermal-films/2.jpeg'),
+      require('../images/thermal-films/3.jpeg'),
+      require('../images/thermal-films/4.jpeg'),
+    ],
+
+    desc: [
+      'It is PET and BOPP based, both side corona treated and one side extrusion coated, thermal laminating film. Film has an extrusion coated surface on PET and BOPP films with low temp. melting resin, which enables the lamination of films to paper, plastic, and many other products by heat and pressure.',
+      'Type of Thermal Films we offer:',
+      '-> Gloss',
+      '-> Matte',
+      '-> Metalized',
+      '-> Holographic',
+    ],
+    advantages: [
+      'Excellent optical properties',
+      'Fibre-tear bond strength',
+      'Excellent esthetic appeal',
+      'Suitable for postpress operation',
+      'Excellent treatment retention on non metallised side',
+      'Excellent tear resistance',
+    ],
+    applications: [
+      'Cards',
+      'Photo Album',
+      'Brochures',
+      'Posters',
+      'Books and Folders',
+      'Cartons',
+    ],
+  },
+];
+
+export default productsData;
